65489adfc15ab93d3119afa41a80dcba925d5c10
htrix <htrix@example.com>
2026-01-03 18:15:01 +0000

    added external secret

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+# Lê as variáveis de ambiente criadas pelo ExternalSecret
+palavra1 = os.environ.get("PALAVRA1", "default1")
+palavra2 = os.environ.get("PALAVRA2", "default2")
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, Tank YOU !!!'
+    return f'Please subscribe, like, and comment on this video !!Segredo 1: {palavra1} | Segredo 2: {palavra2}'
